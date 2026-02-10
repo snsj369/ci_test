d32a77db87c29ec9a4100932bf4b7456f39350aa
lucky <2564851963@qq.com>
2026-02-10 16:15:33 +0000

    feat: 初次提交自动化测试和 CI 配置

---
+++ app.py
@@ -0,0 +1,10 @@
+from urllib import response
+import requests
+
+def check_baidu():
+    "访问百度， 检查返回状态码"
+    response = requests.get("https://www.baidu.com")
+    return response.status_code
+
+
+    
