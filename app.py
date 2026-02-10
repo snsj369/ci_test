1c1bb7b8ebb14129b3c2e5c0cf590b9ae86772b4
lucky <2564851963@qq.com>
2026-02-10 16:40:01 +0000

    bugfix: 修复响应状态bug

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 def check_baidu():
     "访问百度， 检查返回状态码"
-    response = requests.get("https://www.baidu.com/12345")
+    response = requests.get("https://www.baidu.com")
     return response.status_code
 
 
